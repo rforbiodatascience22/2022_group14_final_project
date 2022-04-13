65a7fa71d87b83820daaf2786315e74e98b87333
CathrineLind <s184338@student.dtu.dk>
2022-04-13 14:55:27 +0000

    Add code to 01_load.R and 02_clean.R for loading data

--- R/01_load.R
+++ R/01_load.R
@@ -7,15 +7,15 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-cancerProteosom_raw <- read_tsv(file = "data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
-clinicalData_raw <- read_tsv(file = "data/_raw/clinical_data_breast_cancer.csv")
-PAMproteins_raw <- read_tsv(file = "data/_raw/PAM50_proteins.csv")
+cancerProteosom_raw <- read_csv(file = "data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
+clinicalData_raw <- read_csv(file = "data/_raw/clinical_data_breast_cancer.csv")
+PAMproteins_raw <- read_csv(file = "data/_raw/PAM50_proteins.csv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
+my_data <- cancerProteosom_raw # %>% ...
 
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data,
-          file = "data/01_my_data.tsv")
+          file = "data/01_my_data.csv") #tsv
--- R/02_clean.R
+++ R/02_clean.R
@@ -7,11 +7,14 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+my_data <- read_tsv(file = "data/01_my_data.csv") #tsv
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+my_data_clean <- 
+  my_data %>% 
+  drop_na() 
+  
 
 
 # Write data --------------------------------------------------------------
