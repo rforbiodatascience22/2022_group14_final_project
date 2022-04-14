ed07aefa3c3e69868156067bd7be5504e1b151cc
AllieThe13 <alesiawolsen@gmail.com>
2022-04-14 08:02:25 +0000

    changes names, wrote tsv for 3 datasets

--- R/01_load.R
+++ R/01_load.R
@@ -1,5 +1,6 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("broom")
 
 
 # Define functions --------------------------------------------------------
@@ -7,15 +8,16 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-cancerProteosom_raw <- read_csv(file = "data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
-clinicalData_raw <- read_csv(file = "data/_raw/clinical_data_breast_cancer.csv")
-PAMproteins_raw <- read_csv(file = "data/_raw/PAM50_proteins.csv")
-
-
-# Wrangle data ------------------------------------------------------------
-my_data <- cancerProteosom_raw # %>% ...
+proteomes_raw <- read_csv(file = "data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
+clinical_raw <- read_csv(file = "data/_raw/clinical_data_breast_cancer.csv")
+proteins_raw <- read_csv(file = "data/_raw/PAM50_proteins.csv")
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data,
-          file = "data/01_my_data.csv") #tsv
+write_tsv(x = proteomes_raw,
+          file = "data/proteomes.tsv.gz") #tsv
+write_tsv(x = clinical_raw,
+          file = "data/clinical.tsv.gz") #tsv
+write_tsv(x = proteins_raw,
+          file = "data/proteins.tsv.gz") #tsv
+
