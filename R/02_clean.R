c31da9acfe9ce8480ad67671befea9d99e730bc1
AllieThe13 <alesiawolsen@gmail.com>
2022-04-25 07:38:47 +0000

    Binarisation

--- R/02_clean.R
+++ R/02_clean.R
@@ -40,13 +40,14 @@ clinical_data <- clinical %>%
 clinical_data <- clinical_data %>%
   mutate(Gender = if_else(Gender == "FEMALE", 1, 0),
          ER_Status_bin = if_else(`ER Status` == "Negative", 0, 1),
-         PR_Status_bin = if_else(`PR Status` == "Negative", 0, 1),
-         HER2_Final_Status_bin = if_else(`HER2 Final Status` == "Negative", 0, 1),
+         `PR Status` = if_else(`PR Status` == "Negative", 0, 1),
+         `HER2 Final Status` = if_else(`HER2 Final Status` == "Negative", 0, 1),
          Tumor = as.numeric(str_replace(Tumor, "T", "")),
          Node = as.numeric(str_replace(Node, "N", "")),
-         Metastasis = as.numeric(str_replace(Metastasis, "M", ""))) #%>%
-  #select(., 
-  #       -c(`Tumor--T1 Coded`, `Node-Coded`, `Metastasis-Coded`)) 
+         Metastasis = as.numeric(str_replace(Metastasis, "M", ""))) %>%
+  select(., -c(`Tumor--T1 Coded`, 
+               `Node-Coded`, 
+               `Metastasis-Coded`)) 
 
 view(clinical_data)
 
