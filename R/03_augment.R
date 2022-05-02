d5f2efee078995bb236a1e858781af14d01a75f1
CathrineLind <s184338@student.dtu.dk>
2022-05-02 06:31:53 +0000

    Minor augment update

--- R/03_augment.R
+++ R/03_augment.R
@@ -5,29 +5,25 @@ library("dplyr")
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
+
 # Load data ---------------------------------------------------------------
 
 clinical_data <- read_tsv(file = "data/02_clinical_data.tsv")
-proteome_data <- read_tsv(file = "data/02_proteosome_data.tsv")
+proteome <- read_tsv(file = "data/02_proteosome_data.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
 
-# CLINICALS DATA 
-# Binarize genders and HER2 stores, tumor, node and metastasis change to numeric
-clinical_data <- clinical_data %>%
-  mutate(Gender = case_when(Gender == "FEMALE" ~1, 
-                            Gender == "MALE" ~ 0),
-         HER2_binary = case_when(`HER2 Final Status` == "Negative" ~ 0,
-                                 `HER2 Final Status` == "Positive" ~ 1), #This adds NA for equivocal
-         Tumor = as.numeric(str_replace(Tumor, "T", "")),
-         Node = as.numeric(str_replace(Node, "N", "")),
-         Metastasis = as.numeric(str_replace(Metastasis, "M", ""))) %>%
-  select(., -c(`Tumor--T1 Coded`, 
-               `Node-Coded`, 
-               `Metastasis-Coded`)) 
 
-# Add age groups to clinical data
+# Transpose proteosome data
+prot <- cbind("Complete TCGA ID" = names(proteome), t(proteome))
+
+# Join proteosome and clinical data by "Complete TCGA ID"
+data = clinical %>% left_join(prot, copy = T)
+
+
+
+# Adding Age groups to data
 clinical_data <- clinical_data %>% 
   mutate(Age_groups = case_when(
     30 <= `Age at Initial Pathologic Diagnosis` & `Age at Initial Pathologic Diagnosis`< 40 ~ "30-40",
@@ -38,51 +34,10 @@ clinical_data <- clinical_data %>%
     `Age at Initial Pathologic Diagnosis` >= 80 ~ "80+"))
 
 
-
-# PROTEOMES DATA
-
-# Transpose proteosome data
-prot <- proteome_data %>% 
-  pivot_longer(cols = -c("RefSeq_accession_number"), 
-               names_to = "Complete TCGA ID", 
-               values_to = "value")
-
-prot <- prot %>% 
-  pivot_wider(names_from ="RefSeq_accession_number", 
-              values_from = "value" )
-
-
-# JOINED DATA 
-# Join proteosome and clinical data by "Complete TCGA ID"
-joined_data = clinical_data %>% left_join(prot, copy = T)
-
-
-#view(joined_data)
-#by row 
-#all v-type colums 
-#  if all == NA 
-#    then rm 
-
-data_coloumns <- joined_data %>% 
-  select(matches("V\\d+")) %>%
-  colnames()
-
-
-
-#to check
-temp <- joined_data %>% select(data_coloumns) %>% filter(.,rowSums(is.na(.)) !=ncol(.))
-  
-
-cleaned_joined_data <- joined_data %>% drop_na("NP_958782")
-
-
-    
-# Adding Age groups to data
-
 # Augment data ---------------------------------------------------------
 
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = cleaned_joined_data,
-          file = "data/03_joined_clean_aug_data.tsv")
+write_tsv(x = my_data_clean_aug,
+          file = "data/03_my_data_clean_aug.tsv")
