8a7166c62a86d4165644066344398c253ad8bbdb
AllieThe13 <alesiawolsen@gmail.com>
2022-05-04 09:17:02 +0000

    Heatmap

--- R/heatmap.R
+++ R/heatmap.R
@@ -14,13 +14,51 @@ clean_joined_data <- read_tsv(file = "data/03_joined_clean_aug_data.tsv")
 clean_proteome_data <- read_tsv(file = "data/03_proteome_clean_aug_data.tsv") 
 clean_clinical_data <- read_tsv(file = "data/03_clinical_clean_aug_data.tsv") 
 proteins <- read_tsv(file = "data/proteins.tsv")
+view(clean_joined_data)
 
 # Take inspiration from litrature and look at these spoecific breast cancer genes
-# TP53 = NP_000537
-# PIK3CA
-# GATA3 = NP_001002295
-# ESR1
-# PGR = NP_000917
-# ERBB2 = NP_004439 ( her- 2)
+# PTEN = NP_000305 exist
+# PIK3CA = NP_006209 exist
+# CHEK2 = NP_009125 exist
+# GATA3 = NP_001002295 exist
+# PGR = NP_000917 exist
+# ERBB2 = NP_004439 ( her- 2) exist
+
+
+cancer_genes <- clean_joined_data %>% 
+  select (`Complete TCGA ID`,
+          `PAM50 mRNA`, 
+          Tumor, 
+          NP_000305,
+          NP_006209,
+          NP_009125,
+          NP_001002295, 
+          NP_000917 , 
+          NP_004439) %>% 
+  rename("PTEN" = NP_000305, 
+         "PIK3CA" = NP_006209, 
+         "CHEK2" = NP_009125,
+         "GATA3" = NP_001002295,
+         "PGR" = NP_000917, 
+         "ERBB2" = NP_004439) %>% 
+  pivot_longer(cols = c("PTEN", 
+                        "PIK3CA",
+                        "CHEK2",
+                        "GATA3",
+                        "PGR",
+                        "ERBB2"), 
+               names_to = "Gene", 
+               values_to = "Expression level")
+
+
+cancer_genes %>% 
+  ggplot(mapping = aes(x = Gene,
+                       y = `Complete TCGA ID`,
+                       fill = `Expression level`)) +
+  geom_raster() +
+  scale_fill_gradient2(low = "#075AFF", mid = "white", high = "#FF0000", ) +
+  facet_grid(`PAM50 mRNA`~., scales = "free") +
+  theme_minimal() +
+  theme(axis.text = element_blank())
 
 
